420e7359763b72a3fec5be65a6a848ec803e99d4
Zack Martin <zmartin@outsidesource.com>
2018-12-30 14:42:01 -0500

    Added helper method to get the current principal

--- src/Api/ApiService.cs
+++ src/Api/ApiService.cs
@@ -1,10 +1,19 @@
 ﻿namespace AbstractInterfaces.Api
 {
+	using System.Security.Principal;
+	using System.ServiceModel;
 	using System.ServiceModel.Activation;
 
 	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
 	public abstract class ApiService
 	{
 		//TODO: Try to confine static dependencies (OperationContext, WebOperationContext, HttpContext, etc.) to this class or child services
+
+		protected IPrincipal GetPrincipal()
+		{
+			return OperationContext.Current.IncomingMessageProperties.Security.ServiceSecurityContext.AuthorizationContext.Properties.TryGetValue("Principal", out object obj)
+				? obj as IPrincipal
+				: null;
+		}
 	}
 }
