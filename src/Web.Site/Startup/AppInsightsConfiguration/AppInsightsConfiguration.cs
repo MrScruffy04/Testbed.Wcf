059b4c4b039b9fafafbee4c68a8840e01552ab4d
Zack Martin <zmartin@outsidesource.com>
2019-05-10 15:27:11 -0400

    Now using QuickPulseTelemetryProcessor to collect telemetry

--- src/Web.Site/Startup/AppInsightsConfiguration/AppInsightsConfiguration.cs
+++ src/Web.Site/Startup/AppInsightsConfiguration/AppInsightsConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿namespace AbstractInterfaces.Web.Startup.AppInsightsConfiguration
 {
 	using Microsoft.ApplicationInsights.Extensibility;
+	using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
 	using System.Configuration;
 
 	public static class AppInsightsConfiguration
@@ -10,11 +11,48 @@
 			var config = TelemetryConfiguration.Active;
 
 			config.InstrumentationKey = GetInstrumentationKey();
+
+			AddQuickPulseProcessor(config, GetQuickPulseApiKey());
 		}
 
 		private static string GetInstrumentationKey()
 		{
 			return ConfigurationManager.AppSettings["ApplicationInsights:InstrumentationKey"];
+		}
+
+		private static string GetQuickPulseApiKey()
+		{
+			return ConfigurationManager.AppSettings["ApplicationInsights:ApiKeys:QuickPulse"];
+		}
+
+		private static void AddQuickPulseProcessor(TelemetryConfiguration config, string apiKey)
+		{
+			QuickPulseTelemetryProcessor processor = null;
+
+			config.TelemetryProcessorChainBuilder
+				.Use(next =>
+				{
+					processor = new QuickPulseTelemetryProcessor(next);
+
+					return processor;
+				})
+				.Build();
+
+			var module = new QuickPulseTelemetryModule
+			{
+				AuthenticationApiKey = apiKey,
+			};
+
+			module.Initialize(config);
+			module.RegisterTelemetryProcessor(processor);
+
+			foreach (var tp in config.TelemetryProcessors)
+			{
+				if (tp is ITelemetryModule tm)
+				{
+					tm.Initialize(config);
+				}
+			}
 		}
 	}
 }
